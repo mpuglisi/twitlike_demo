2ad13be2aaea9277392f9a7bcd492ab23aa27408
Massimo Puglisi <kmaxim25@gmail.com>
2013-03-16 20:16:20 +0100

    Primo Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitlikeDemo::Application.config.secret_token = '6ce422525ac66b275a485beee3362b90ac0c010993344ac2981e30e52cf01e2fc0b94c9e7ed88747add93d5f863755dbf8621011b680169423f661ed60318122'
